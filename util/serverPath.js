d44999d0b8b1900bf3ff80b939fa8d6ab72b587f
tikkkkkkkkk <97126942+tikkkkkkkkk@users.noreply.github.com>
2022-01-06 00:57:58 +0530

    Update serverPath.js

--- util/serverPath.js
+++ util/serverPath.js
@@ -1,3 +1,3 @@
-exports.serverPath = "https://riztik.in"; // example: https://example.example.com/
+exports.serverPath = "https://68.183.88.158"; // example: https://example.example.com/
 
 exports.serverKey = "AAAAGlcAYNw:APA91bF9PlposxY3usTFeG0xoMnyg38rnkB22k-OFi6FXzPAhvjp1s0prUEWseKPb8dXWkxvc3ZsLpgz7qbAgYfgh8YS5hoaIBWQD2YqDSwIpbnfC08IBpKf7EpLptkwU8l7rI1f22-y"; //get this from firebase
