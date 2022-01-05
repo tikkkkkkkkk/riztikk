ad014ad9c22bf18d20d93fc2f48bb8492b6ecbab
Your Name <you@example.com>
2022-01-05 17:48:01 +0530

    initial commit of reactjs web messenger project

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //port
+  PORT: process.env.PORT || 5000,
+
+  //secret key
+  secret_key: "THIS_IS_YOUR_SECRET_KEY",
+};
---
+++ util/serverPath.js
@@ -0,0 +1,3 @@
+exports.serverPath = "https://riztik.in"; // example: https://example.example.com/
+
+exports.serverKey = "AAAAGlcAYNw:APA91bF9PlposxY3usTFeG0xoMnyg38rnkB22k-OFi6FXzPAhvjp1s0prUEWseKPb8dXWkxvc3ZsLpgz7qbAgYfgh8YS5hoaIBWQD2YqDSwIpbnfC08IBpKf7EpLptkwU8l7rI1f22-y"; //get this from firebase
